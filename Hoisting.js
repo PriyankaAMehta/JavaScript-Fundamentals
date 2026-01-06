38fba0a76f8e576b1e0eeb5567c17dc16efb1598
PriyankaAMehta <prayankagm@gmail.com>
2026-01-06 16:11:42 +0100

    other way of writing function and checking the hoisting

--- Hoisting.js
+++ Hoisting.js
@@ -1,5 +1,5 @@
 //How to run the file ? : node Hoisting.js
-
+getName2();
 getName();
 console.log(getName);
 console.log(x);
@@ -7,5 +7,9 @@ console.log(x);
 var x = 7;
 
 var getName = () => {
+  return console.log("JavaScript Series");
+};
+
+var getName2 = function () {
   return console.log("JavaScript Series");
 };
