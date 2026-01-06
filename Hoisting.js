72472de942c7b5cc69b783b2efa1ed1d446adc9c
PriyankaAMehta <prayankagm@gmail.com>
2026-01-06 16:03:49 +0100

    writing the normal function as arrow function as example of hoisting

--- Hoisting.js
+++ Hoisting.js
@@ -1,7 +1,11 @@
 //How to run the file ? : node Hoisting.js
+
 getName();
+console.log(getName);
 console.log(x);
 
-function getName() {
+var x = 7;
+
+var getName = () => {
   return console.log("JavaScript Series");
-}
+};
