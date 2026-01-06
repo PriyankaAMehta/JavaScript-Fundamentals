c5b7c51a601f56f04cdb03ddcc28d3a3dab027bb
PriyankaAMehta <prayankagm@gmail.com>
2026-01-06 14:51:24 +0100

    Hoisting demonstration part 1 with simple programme

---
+++ Hoisting.js
@@ -0,0 +1,15 @@
+//How to run the file ? : node Hoisting.js
+// Simple programme
+var x = 7;
+
+function getName() {
+  return console.log("JavaScript Series");
+}
+
+getName();
+console.log(x);
+
+/* OUTPUT
+JavaScript
+7 
+*/
